3baaf0168f456878e3a9ac670e1b6dee91284984
Himasaila Gonugunta <63280027+himasaila111@users.noreply.github.com>
2024-05-17 17:48:54 -0400

    Update auth.js

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -3,7 +3,11 @@ import express from 'express';
 import { register, login } from '../Controllers/authController.js';
 
 const router = express.Router();
-
+router.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
 router.post('/register', register);
 router.post('/login', login);
 
