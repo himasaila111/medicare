39c64de1d9d89f08688686d8f8ff02a97d01a87a
Himasaila Gonugunta <63280027+himasaila111@users.noreply.github.com>
2024-05-17 17:24:58 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: ["https://medicare-app-zeta.vercel.app"],
+  origin: "https://medicare-app-zeta.vercel.app",
 methods: ["POST", "GET"],
 credentials: true
 };
